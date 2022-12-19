9001221c6b106e3a46d39dc06ddd0b9d3ddd103a
Robouste <b>
2022-12-19 17:36:15 +0100

    - fix config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import "phaser";
 import { Gameconfig } from "./game/helpers/types";
 import { GameScene } from "./game/scenes/game.scene";
+import { PreloadScene } from "./game/scenes/preload.scene";
 
 const config: Gameconfig = {
 	width: 800,
 	height: 600,
 	parent: "thegame",
-	scene: GameScene,
+	scene: [PreloadScene, GameScene],
 };
 
 new Phaser.Game(config);
