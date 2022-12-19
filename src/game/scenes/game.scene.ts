f6cf6cc28bf73b6f895a310f421201f882895689
Robouste <b>
2022-12-19 17:32:21 +0100

    - move preload to preload scene

--- src/game/scenes/game.scene.ts
+++ src/game/scenes/game.scene.ts
@@ -7,12 +7,10 @@ export class GameScene extends Phaser.Scene {
 		super(Keys.Scenes.Game);
 	}
 
-	public preload(): void {
-		this.load.image("logo", "assets/phaser3-logo.png");
-	}
 	public create(): void {
 		this.image = this.add.image(400, 300, "logo");
 	}
+
 	public update(): void {
 		this.image.rotation += 0.01;
 	}
--- src/game/scenes/preload.scene.ts
+++ src/game/scenes/preload.scene.ts
@@ -4,7 +4,10 @@ export class PreloadScene extends Phaser.Scene {
 	constructor() {
 		super(Keys.Scenes.Preload);
 	}
-	public preload(): void {}
+
+	public preload(): void {
+		this.load.image("logo", "assets/phaser3-logo.png");
+	}
 
 	public create(): void {
 		this.scene.start(Keys.Scenes.Game);
